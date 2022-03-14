74d7efa6a264c9c701acb4e4b6887c2404a50918
Fogete <74245508+Fogete@users.noreply.github.com>
2022-03-14 22:48:34 +0100

    feat: added swap

--- contracts/OTCTrader.sol
+++ contracts/OTCTrader.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0
-// Feel free to change the license, but this is what we use
 
-// Feel free to change this version of Solidity. We support >=0.6.0 <0.7.0;
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 import {SafeERC20, SafeMath, IERC20, Address} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-//contract to swap woofy for yfi. 1-1
+// Contract to swap woofy for yfi. 1-1
 contract OTCTrader is Ownable {
     using SafeERC20 for IERC20;
     using Address for address;
@@ -17,8 +15,7 @@ contract OTCTrader is Ownable {
     address internal constant yfi = 0x29b0Da86e484E1C0029B56e817912d778aC0EC69;
     address internal constant woofy = 0xD0660cD418a64a1d44E9214ad8e459324D8157f1;
     mapping(address => uint256) public deposits; // amount of liquidity user has provided
-    mapping(address => bool) public traders; // allow us to limit traders
-
+    mapping(address => bool) public traders; // allows us to limit traders
 
     constructor() public {
         traders[msg.sender] = true;
@@ -31,7 +28,7 @@ contract OTCTrader is Ownable {
         traders[_trader] = _allowed;
     }
 
-    //trade between yfi and woofy at 1-1
+    // Trade between yfi and woofy at 1-1
     function trade(address _tokenIn, uint256 _amount) public tradersonly {
         require(_tokenIn == yfi || _tokenIn == woofy, "token not allowed");
 
@@ -51,7 +48,7 @@ contract OTCTrader is Ownable {
         tokenOut.safeTransfer(msg.sender, _amount);
     }
 
-    //provide liquidity with yfi or woofy. treated as the same
+    // Provide liquidity with yfi or woofy. Treated as the same.
     function provideLiquidity(address _token, uint256 _amount) public tradersonly {
         require(_token == yfi || _token == woofy, "token not allowed");
         deposits[msg.sender] = deposits[msg.sender].add(_amount);
@@ -63,7 +60,6 @@ contract OTCTrader is Ownable {
         uint256 diff = token.balanceOf(address(this)).sub(balanceBefore);
 
         require(diff >= _amount);
-        
     }
 
     function withdrawLiquidity(address _token, uint256 _amount) public tradersonly {
@@ -79,16 +75,11 @@ contract OTCTrader is Ownable {
         
         require(deposited >= _amount, "not enough deposits");
 
-        token.safeTransfer(msg.sender, _amount);
-
-        
+        token.safeTransfer(msg.sender, _amount);        
     }
 
     modifier tradersonly() {
-
         require(traders[msg.sender], "traders only");
-   
-
         _;
     }
 }
